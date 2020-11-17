11769dd9f7745f99cb695947424cd565f493e490
Ryan <Ryan.sc.shz@gmail.com>
2020-11-18 01:41:53 +0800

    完善react-todolist示例，增加知识点应用

--- react-sample/todo-list/src/TodoItem.js
+++ react-sample/todo-list/src/TodoItem.js
@@ -1,14 +1,16 @@
 import React from "react";
+// 使用prop-types对props中的参数做校验
+import PropTypes from 'prop-types';
 
 class TodoItem extends React.Component {
   render() {
     // 解构赋值 props 里的数据
-    let { item, itemClick } = this.props;
+    let { item, itemClick, index } = this.props;
     return (
       // 使用父组件传递过来的数据和方法
       <li
         onClick={() => {
-          itemClick(this.props.index);
+          itemClick(index);
         }}
       >
         {/* 读取props中的值 */}
@@ -16,6 +18,18 @@ class TodoItem extends React.Component {
       </li>
     );
   }
+}
+
+// 声明参数类型，如果类型传错会抛出警告
+TodoItem.propTypes = {
+  item: PropTypes.string.isRequired, // isRequired表示这个参数一定要传
+  itemClick: PropTypes.func,
+  index: PropTypes.number
+} 
+
+// 声明属性的默认值
+TodoItem.defaultProps = {
+  item: 'Yes'
 }
 
 export default TodoItem;
--- react-sample/todo-list/src/TodoList.js
+++ react-sample/todo-list/src/TodoList.js
@@ -1,21 +1,27 @@
 import React from "react";
-
-// 导入css文件
-import "./style.css";
-
-import "./TodoItem";
 import TodoItem from "./TodoItem";
+// 导入css文件一般放到最后
+import "./style.css";
 
 // 定义一个组件
 class TodoList extends React.Component {
   // 构造函数是固定写法
   constructor(props) {
     super(props);
-    // 定义数据，必须放在状态中
+    // 定义数据，必须放在state中
     this.state = {
       inputValue: "",
       list: [],
     };
+
+    // 进阶：
+    // 1. 当组件的state或者props发生改变时，render函数就会重新执行
+    // 2. 当父组件的render函数被执行时，子组件的render函数也会重新执行
+
+    // 将函数的this绑定提前可以优化页面性能
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.handleLiItemClick = this.handleLiItemClick.bind(this);
   }
 
   render() {
@@ -29,60 +35,91 @@ class TodoList extends React.Component {
           id="textArea"
           className="input"
           value={this.state.inputValue}
-          onChange={this.handleInputChange.bind(this)}
+          onChange={this.handleInputChange}
+          // 使用ref可以获取到对应的dom元素来操作，但不推荐
+          // 因为setState是一个异步函数，我们直接使用ref去操作dom元素可能并不是最新的
+          // 我们应该更多的使用数据驱动的形式
+          ref={(input) => {
+            this.input = input;
+          }}
         />
-        <button onClick={this.handleButtonClick.bind(this)}>提交</button>
-        <ul>
-          {/* 在JSX中使用语句，并返回一个li */}
-          {this.state.list.map((item, index) => {
-            return (
-              <div>
-                {/* <li
-                  key={index}
-                  onClick={this.handleLiItemClick.bind(this, index)}
-                  // 使用 dangerouslySetInnerHTML 来设置元素的 innerHtml 属性，可以item中的html标签也生效
-                  // 比如 item 的值是 <h1>Hello</h1>
-                  dangerouslySetInnerHTML={{ __html: item }}
-                ></li> */}
-                <TodoItem item={item} index={index} itemClick={this.handleLiItemClick.bind(this)}/>
-              </div>
-            );
-          })}
-        </ul>
+        <button onClick={this.handleButtonClick}>提交</button>
+        <ul>{this.getItems()}</ul>
       </React.Fragment>
     );
   }
 
-  handleInputChange(e) {
-    // 获取变化后的input框中的值
-    console.log(e.target.value);
-    // React 中不能直接通过this.state去修改页面数据
-    // this.state.inputValue = e.target.value;
-    // 必须通过 this.setState去修改
-    this.setState({
-      inputValue: e.target.value,
+  getItems() {
+    return this.state.list.map((item, index) => {
+      return (
+        // 列表中的key需要加载每个item的最外层，且不要用index
+        <div key={item}>
+          {/* 在JSX中使用语句，并返回一个li */}
+          {/* <li
+            key={index}
+            onClick={this.handleLiItemClick.bind(this, index)}
+            // 使用 dangerouslySetInnerHTML 来设置元素的 innerHtml 属性，可以item中的html标签也生效
+            // 比如 item 的值是 <h1>Hello</h1>
+            dangerouslySetInnerHTML={{ __html: item }}
+          ></li> */}
+
+          {/* 
+            1. 需要注意的是：React中的数据是单向数据流，父组件传递给子组件的数据仅仅是可读的
+            2. 也就是说，子组件使用父组件传递的数据时不能去修改
+          */}
+          <TodoItem
+            item={item}
+            index={index}
+            itemClick={this.handleLiItemClick}
+          />
+        </div>
+      );
     });
+  }
+
+  handleInputChange(e) {
+    // 通过e.target获取到产生事件的元素，是个DOM节点
+    console.log(e.target);
+    // 获取变化后的input框中的值
+    // React中不要直接通过this.state去修改状态数据
+    // this.state.inputValue = e.target.value;
+    // 应该通过 this.setState去修改
+    // React16之后推荐在setState后传入返回新state对象的函数，因为setState是一个异步函数！
+    // setState有两个函数类型的参数，第二个是异步完成后的回调
+    this.setState(
+      () => ({
+        inputValue: e.target.value,
+      }),
+      () => {
+        console.log("setState is execute complete.");
+      }
+    );
   }
 
   // 添加一个任务到todo-list中
   handleButtonClick() {
     console.log("handle click");
-    // 从 this.state中读取数据
-    // 拷贝一个新的数组来修改，而不要使用原有的数组
-    let list = [...this.state.list, this.state.inputValue];
-    this.setState({
-      list,
-      inputValue: "",
+    // setState传入的函数有一个prevState参数, 表示之前的状态
+    this.setState((prevState) => {
+      // 从prevState中读取数据
+      // 拷贝一个新的数组来修改，而不要使用原有的数组
+      let list = [...prevState.list, prevState.inputValue];
+      return {
+        list,
+        inputValue: "",
+      };
     });
   }
 
   // 删除一个任务
   handleLiItemClick(index) {
     console.log(`item index is ${index}`);
-    let list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState({
-      list,
+    this.setState((prevState) => {
+      let list = [...prevState.list];
+      list.splice(index, 1);
+      return {
+        list,
+      };
     });
   }
 }
