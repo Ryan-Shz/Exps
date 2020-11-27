c220fbdb8af9c223ce363a2076552df133a6a96e
Ryan <Ryan.sc.shz@gmail.com>
2020-11-27 18:34:01 +0800

    vue添加路由和props

--- vue-sample/basic-sample/src/main.js
+++ vue-sample/basic-sample/src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+// 使用vue-router
+import VueRouter from 'vue-router';
+Vue.use(VueRouter)
+
+// 导入路由对应组件
+import Props from './components/props'
+import Slot from './components/slot'
+
+// 定义路由
+const routes = [
+  {path: '/slot', component: Slot},
+  {path: '/props', component: Props}
+]
 
 Vue.config.productionTip = false
 
