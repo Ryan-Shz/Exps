68884280f77a732cdde5e6a8e5d46e40f8ebcc87
Ryan <Ryan.sc.shz@gmail.com>
2020-11-27 17:51:33 +0800

    vue添加slot使用案例

---
+++ vue-sample/basic-sample/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <slot-component>
+      这里是slot内容
+      <!-- #header 是 v-slot:header 的缩写 -->
+      <template #header>自定义Header</template>
+      <!-- 默认插槽，即没有给slot指定name属性的插槽 -->
+      <template #default>
+        <ul>
+          <li>123</li>
+          <li>456</li>
+          <!-- 可以访问当前组件的数据 -->
+          <li>{{ name }}</li>
+        </ul>
+      </template>
+      <template v-slot:footer>
+        这里是Footer
+      </template>
+      <!-- 作用域插槽，使用插槽绑定的数据 -->
+      <template #scope="scopeProps"> {{ scopeProps.content }}123 </template>
+      <!-- 解构插槽的prop -->
+      <template #desconstruct="{content}">
+        {{content}}
+      </template>
+    </slot-component>
+  </div>
+</template>
+
+<script>
+import SlotComponent from "./components/SlotComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    SlotComponent,
+  },
+  data() {
+    return {
+      name: "Ryan",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue-sample/basic-sample/src/components/SlotComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    这个一个slot示例组件
+    <h1><slot name="header">默认Header</slot></h1>
+    <h2>
+      <!-- 后备内容 -->
+      <slot>HelloWorld</slot>
+    </h2>
+    <slot name="footer"></slot>
+    <h1>
+      <slot name="scope" :content="content">{{ content }}</slot>
+    </h1>
+    <h2>
+      <slot name="desconstruct" :content="content">{{ content }}</slot>
+    </h2>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      content: "这个是作用域插槽",
+    };
+  },
+};
+</script>
+<style scoped></style>
